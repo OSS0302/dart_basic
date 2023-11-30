2ec4231ebc8b32b48e391dc127f1bf9e8036bf3a
Ohsangsoo <tkdtn3264@gmail.com>
2023-11-30 20:16:38 +0900

    성직자 클레릭 클래스 작성(연습문제 1-3)

--- lib/23_11_30/class/cleric.dart
+++ lib/23_11_30/class/cleric.dart
@@ -1,4 +1,7 @@
-main() {}
+main() {
+  Cleric cleric = Cleric('cleric', 50, 10);
+  cleric.selfAid(2);
+}
 
 // 클레릭 성직자 클래스
 class Cleric {
@@ -15,5 +18,14 @@ class Cleric {
 
   Cleric( this.name, this.hp, this.mp );
 
+  //selfAid() 메소드
+  void selfAid(int mp) {
 
+    if(mp < 10 && mp >= 5){
+        hp =50;
+        mp =mp -5;
+    }else if( mp < 5){
+        print('마력이 부족 합니다.');
+    }else{}
+  }
 }
