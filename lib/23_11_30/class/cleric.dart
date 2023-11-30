a99188767c8518bef6ffd9a0172765c5123f75c7
Ohsangsoo <tkdtn3264@gmail.com>
2023-11-30 18:51:37 +0900

    히어로 클래스 작성

--- lib/23_11_30/class/cleric.dart
+++ lib/23_11_30/class/cleric.dart
@@ -1,4 +1,20 @@
 main(){}
 
+//1-3
+// 성직자는 “셀프 에이드" 라는 마법을 사용할 수 있고, MP를 5소비하는 것으로 자신의 HP 를 최대 HP 까지 회복할 수 있다.
+// 연습 1-2 에 선언한 Cleric 클래스에 “selfAid()” 메소드를 추가 하시오.
+// 또한, 이 메소드는 인수가 없고, 리턴 값도 없다
+
 //  클레릭 성직자 클래스
-class Cleric{}
+class Cleric{
+  String name;
+  final int hp =50;
+  final int mp =10;
+
+  Cleric(this.name);
+  //selfAid()” 메소드
+  void selfAid(){
+    int ;
+    int hp = 50;
+  }
+}
---
+++ lib/23_11_30/class/hreo.dart
@@ -0,0 +1,10 @@
+main(){}
+
+class Hero {
+  final String name; // 이름
+  final int hp; // 체력
+  final int mp; // 마력
+
+  Hero(this.name, this.hp, this.mp);
+
+}
