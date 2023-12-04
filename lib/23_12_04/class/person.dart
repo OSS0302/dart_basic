0570c87bf3ad7459fd4d02e1f788a32858a95c9b
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-04 23:13:31 +0900

    연습문제 2-2 완료

--- lib/23_12_04/class/person.dart
+++ lib/23_12_04/class/person.dart
@@ -1,6 +1,7 @@
 void main(){
   final Person person = Person(name: '홍길동');
     person.name = 'a';
+
 }
 
 class Person{
@@ -16,7 +17,7 @@ class Person{
   // setter  타탕성 검사
   set name(String value) {
     if(value.length <= 1){
-      throw Exception('에러발생 ');
+      throw Exception('에러발생!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ');
     }
     _name = value;
   }
