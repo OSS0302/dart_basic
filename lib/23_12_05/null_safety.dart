079f0ecf8bfd573df604f80076a788a4a2b33251
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-05 14:41:07 +0900

    개발자 널 보증 하는 !는 사용하지 말자

---
+++ lib/23_12_05/null_safety.dart
@@ -0,0 +1,16 @@
+void nain(){
+//   //non nullable
+//   int num =10;
+//   //null able
+//   int? num2;
+//
+//   개발자 널 보증 하는 !는 사용하지 말자
+//   num1 =num2!;
+//   num2 =num2;
+// }
+// class Person{
+//   String? name;
+//   String name2;
+
+
+}
