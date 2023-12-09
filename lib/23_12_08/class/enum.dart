ffce933b11c37998f7539375677cf005a42bd88e
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-09 09:55:29 +0900

    enum 수업 내용 연습하기

---
+++ lib/23_12_08/class/enum.dart
@@ -0,0 +1,19 @@
+
+enum Color {
+  red,
+  blue,
+  bleak,
+}
+Color color = Color.bleak;
+  int colors = 1;
+void main(){
+  switch(colors){
+    case 1:
+      print('bleak');
+    case 2:
+      print('blue');
+    case 3:
+      print('red');
+  }
+S
+}
