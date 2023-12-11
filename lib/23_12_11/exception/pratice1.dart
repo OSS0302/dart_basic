5ce3c2829a429ea13dc5a40be1a6a7ab1e3179e4
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-11 23:10:30 +0900

    연습문제2

--- lib/23_12_11/exception/pratice1.dart
+++ lib/23_12_11/exception/pratice1.dart
@@ -1,9 +1,22 @@
-
 void main() {
-  final numString = '10.5';
+// 함수 호출
+  print(someError());
 
-  int num = int.parse(numString);
+  try {
+    final numString = '10.5';
 
-//출력하기
-  print(num);
+    int num = int.parse(numString);
+
+    //출력하기
+    print(num);
+  } catch (e) {
+    print(e);
+  }
+}
+
+int someError() {
+  int num = 0;
+  // throw FormatException('예외가 발생하면 0 으로 처리');
+
+  return num;
 }
