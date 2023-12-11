61ed4dc76fc70bc283e081628d177a06b5ad653e
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-11 21:57:14 +0900

    파일 다루기 연습문제

--- lib/23_12_11/exception/pratice1.dart
+++ lib/23_12_11/exception/pratice1.dart
@@ -1,7 +1,22 @@
+void main() {
+// 함수 호출
+  print(someError());
 
-final numString = '10.5';
+  try {
+    final numString = '10.5';
 
-int num = int.parse(numString);
+    int num = int.parse(numString);
 
-//출력하기
-print(num);
+    //출력하기
+    print(num);
+  } catch (e) {
+    print(e);
+  }
+}
+
+int someError() {
+  int num = 0;
+  // throw FormatException('예외가 발생하면 0 으로 처리');
+
+  return num;
+}
